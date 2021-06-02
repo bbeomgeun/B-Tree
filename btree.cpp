511651dff5716479e01956a7ba182a2775ddcace
bbeomgeun <miki308@naver.com>
2021-06-02 23:59:48 +0900

    creation

--- btree.cpp
+++ btree.cpp
@@ -1,15 +1,25 @@
 // 12161104 박범근 데이터베이스 b+tree 구현 과제
 
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 
 using namespace std;
 
 class BTree {
 public:
-	BTree(const char* fileName, int blockSize) {
+	BTree(const char *fileName, int blockSize) {
 
 	}
+
+	void creation(const char* fileName, int blockSize) {
+		ofstream binaryFile("btree.bin", ios::out | ios::binary);
+		//헤더 write (blockSize, rootBID = 1, Depth = 0)
+		binaryFile.write(reinterpret_cast<char*>(&blockSize), sizeof(int));
+		binaryFile.write(reinterpret_cast<char*>(&blockSize), sizeof(int));
+		binaryFile.write(reinterpret_cast<char*>(&blockSize), sizeof(int));
+	}
+
 	bool insert(int key, int rid) {
 
 	}
@@ -17,15 +27,17 @@ public:
 	
 	}
 	int* pointSearch(int key) {
+
 	}
 	int* rangeSearch(int startRange, int endRange) {
+
 	}
 };
 // Test
 int main(int argc, char* argv[])
 {
 	char command = argv[1][0];
-	BTree myBtree = new BTree(any parameter);
+	BTree * myBtree = new BTree();
 
 	switch (command)
 	{
