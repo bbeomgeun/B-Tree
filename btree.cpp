18283956b16460fc3f328ec8cda6d43ad9544c1c
bbeomgeun <miki308@naver.com>
2021-06-17 17:10:08 +0900

    insert 이후 leafSplit, nonLeafSplit 완성 todo : sampleCase 돌려보면서 디버깅 이후 나머지 search, print 완료

--- btree.cpp
+++ btree.cpp
@@ -64,7 +64,11 @@ public:
 	}
 };
 
-bool compare(DataEntry* a, DataEntry* b) {
+bool compareLeaf(DataEntry* a, DataEntry* b) {
+	return a->key < b->key;
+}
+
+bool compareNonLeaf(IndexEntry* a, IndexEntry* b) {
 	return a->key < b->key;
 }
 
@@ -109,6 +113,13 @@ public:
 		fseek(pFile, 4, SEEK_SET);
 		fwrite(&rootBID, sizeof(int), 1, pFile);
 		fwrite(&depth, sizeof(int), 1, pFile);
+		fclose(pFile);
+	}
+
+	void setLeafNextBID(int rootBID, int nextBID) {
+		pFile = fopen(this->btreeFileName, "r+b");
+		fseek(pFile, getBlockOffset(rootBID+1)-4, SEEK_SET);
+		fwrite(&nextBID, sizeof(int), 1, pFile);
 		fclose(pFile);
 	}
 
@@ -185,20 +196,62 @@ public:
 	}
 
 	bool isLeafSplit(LeafNode * temp) {
-		return temp->dataEntries.size()+1 > getEntryPerNode(); // 1개 entry 삽입되면 split이 일어나는지 체크
+		return temp->dataEntries.size() > getEntryPerNode(); // 현재 갯수로 split이 일어나는지 체크
 	}
 	bool isNonLeafSplit(NonLeafNode * temp) {
-		return temp->indexEntries.size()+1 > getEntryPerNode(); // 1개 entry 삽입되면 split이 일어나는지 체크
+		return temp->indexEntries.size() > getEntryPerNode(); // 1개 entry 삽입되면 split이 일어나는지 체크
 	}
-	void leafSplit() {
-	}
-	void nonLeafSplit() {
+	IndexEntry*  leafSplit(int insertLocation, LeafNode * tempLeafNode) {
+		int splitBlockID = this->blockCount + 1;
+		makeNewBlock(splitBlockID); // 새 splitNode 생성
+		tempLeafNode->nextLeafNode = splitBlockID; // 기존 노드에 nextLeaf 연결
+		setLeafNextBID(insertLocation, splitBlockID);  // 기존 노드 파일에 nextLeaf 파일 write
+		// nextBID 지정, splitNode에 반절 data 옮기기, 상위 노드에 pointer 추가하기
+		LeafNode* splitLeafNode = new LeafNode(); // 새 splitNode
+		int leftSize = tempLeafNode->dataEntries.size(); // 기존 노드 사이즈, 이미 추가된 상태라 (제한 + 1개 추가 entry)
+		// 데이터 옮기는 과정
+		for (int i = leftSize / 2; i < leftSize; i++) {
+			splitLeafNode->dataEntries.push_back(tempLeafNode->dataEntries[i]);
+		} // 반을 옮기고 기존은 반부터 끝까지 삭제한다.
+		tempLeafNode->dataEntries.erase(tempLeafNode->dataEntries.begin() + leftSize / 2, tempLeafNode->dataEntries.end());
+		updateLeafData(insertLocation, tempLeafNode); // 파일에 변경된 기존 노드 작성(내부에서 0으로 초기화되고 write)
+		updateLeafData(splitBlockID, splitLeafNode); // 파일에 splitNode 작성(내부에서 0으로 초기화되고 write)
+		//---------------------여기까지 리프노드 split -> blockCount만 늘어나고 root나 depth는 관련 X
+		int newIndexKey = splitLeafNode->dataEntries.front()->key; // 포인터용 key + blockID는 splitBlockID 사용
+		IndexEntry * newIndexEntry = new IndexEntry(newIndexKey, splitBlockID);
+
+		return newIndexEntry;
 	}
 
-	void updateLeafData(int blockID, LeafNode* _leafNode) { // writeFile -> key와 value만 받으면 해당 block에 입력해주는 함수
+	IndexEntry*  nonLeafSplit(int tempUpBlockID, NonLeafNode * tempNonLeafNode) {
+		int splitBlockID = this->blockCount + 1;
+		makeNewBlock(splitBlockID);
+		NonLeafNode* splitNonLeafNode = new NonLeafNode();
+		int leftSize = tempNonLeafNode->indexEntries.size();
+		IndexEntry* newIndexEntry = new IndexEntry(tempNonLeafNode->indexEntries[leftSize]->key, splitBlockID);
+		//위로 올라갈 nonLeafEntry(오른쪽 첫번째 key값, 오른쪽 new blockID)
+		// 추가로 오른쪽 새로 생긴 블럭의 blockID를 지정해줘야한다.
+		splitNonLeafNode->BIDpointer = tempNonLeafNode->indexEntries[leftSize]->BIDpointer;
+		//올라가는 entry가 가리키고 있던 하위레벨 blockID를 splitNode가 가리키고 올라가는 entry는 이 block을 가리킴
+		for (int i = leftSize / 2+1; i < leftSize; i++) { // 올라가는 entry 제외하고 옮겨준다.
+			splitNonLeafNode->indexEntries.push_back(tempNonLeafNode->indexEntries[i]);
+		}
+		tempNonLeafNode->indexEntries.erase(tempNonLeafNode->indexEntries.begin() + leftSize / 2, tempNonLeafNode->indexEntries.end());
+		updateNonLeafData(tempUpBlockID, tempNonLeafNode);
+		updateNonLeafData(splitBlockID, splitNonLeafNode);
+
+		return newIndexEntry;
+	}
+
+	void updateLeafData(int blockID, LeafNode* _leafNode) {
+		// writeFile -> 해당 블럭을 다 지우고, Entry에 해당하는 key value를 모두 다시 작성
 		pFile = fopen(this->btreeFileName, "r+b"); // write binary
-		//fseek(pFile, getBlockOffset(blockID) + _leafNode->location, SEEK_SET); // + 블럭시작부터가 아니라 데이터가 없는 곳부터 넣어야할듯.
 		fseek(pFile, getBlockOffset(blockID), SEEK_SET); // 정렬된 순서를 write해야하므로 매번 block처음부터 wirte해주자.
+		int bufferSize = getNumberPerNode();
+		int* bufferArray = new int[bufferSize](); // 동적할당 및 0으로 초기화
+		fwrite(&bufferArray, sizeof(int), bufferSize, pFile); // blockSize(byte)만큼 0으로 초기화
+		delete[] bufferArray;
+		fseek(pFile, getBlockOffset(blockID), SEEK_SET); // 다시 offset으로 가서 새로운 entry 추가 (현재 0으로 초기화)
 		for (int i = 0; i < _leafNode->dataEntries.size(); i++) { // 새로운 entry가 추가 +정렬되어 있는 상황. 파일 업데이트
 			int key = _leafNode->dataEntries[i]->key;
 			int value = _leafNode->dataEntries[i]->value;
@@ -208,8 +261,22 @@ public:
 		fclose(pFile);
 	}
 
-	void updateNonLeafData(int blockID, NonLeafNode * _nonLeafNode) { // split시 이용
-
+	void updateNonLeafData(int blockID, NonLeafNode * _nonLeafNode) { // split시 이용(0으로 다 지우고 다시 write)
+		pFile = fopen(this->btreeFileName, "r+b"); // write binary
+		fseek(pFile, getBlockOffset(blockID), SEEK_SET); // 정렬된 순서를 write해야하므로 매번 block처음부터 wirte해주자.
+		int bufferSize = getNumberPerNode();
+		int* bufferArray = new int[bufferSize](); // 동적할당 및 0으로 초기화
+		fwrite(&bufferArray, sizeof(int), bufferSize, pFile); // blockSize(byte)만큼 0으로 초기화
+		delete[] bufferArray;
+		fseek(pFile, getBlockOffset(blockID), SEEK_SET); // 다시 offset으로 가서 다시 작성
+		fwrite(&_nonLeafNode->BIDpointer, sizeof(int), 1, pFile); // nonLeafNode는 BIDPointer부터
+		for (int i = 0; i < _nonLeafNode->indexEntries.size(); i++) { // 새로운 entry가 추가 +정렬되어 있는 상황. 파일 업데이트
+			int key = _nonLeafNode->indexEntries[i]->key;
+			int BIDpointer = _nonLeafNode->indexEntries[i]->BIDpointer;
+			fwrite(&key, sizeof(int), 1, pFile);
+			fwrite(&BIDpointer, sizeof(int), 1, pFile);
+		}
+		fclose(pFile);
 	}
 
 	void creation(string fileName, int blockSize) { // 헤더 write (blockSize, rootBID = 1, Depth = 0)
@@ -225,26 +292,66 @@ public:
 	}
 
 	void insert(int key, int value) {
-		stack<int> trackID;
-		trackID = searchBlock(key); // 삽입해야할 block 찾기 및 찾아온 경로
-		int insertLocation = trackID.top();
-		trackID.pop();
-		if (getHeader(Header::rootBID) == 0) { // 루트가 없는 경우는 루트1 + block1생성
+		if (getHeader(Header::rootBID) == 0) { // 루트가 없는 경우는 setRoot(1) + block1생성
 			int newRoot = 1;
 			setHeader(newRoot, 0); // 루트 blockID 1로 update
 			makeNewBlock(newRoot); // block #1 생성
 		} // 여기까지 트리 첫 초기화 과정(아무 블럭도 없을 경우)
 		 // 초기화해주고나서 삽입과정 시작
-		int tempRootBID = getHeader(Header::rootBID);
-		LeafNode* tempLeafNode = getLeafNode(tempRootBID); // 해당 노드를 찾아서 LeafNode에 저장(dataEntry + nextBID)
-		if (isLeafSplit(tempLeafNode)) { // 1개 추가하면 blockSize를 넘는 경우
+		stack<int> trackID;
+		trackID = searchBlock(key); // 삽입해야할 block 찾기 및 찾아온 경로
+		int insertLocation = trackID.top();
+		trackID.pop();
+		LeafNode* tempLeafNode = getLeafNode(insertLocation); // 해당 노드를 찾아서 LeafNode에 저장(dataEntry + nextBID)
+		DataEntry* newDataEntry = new DataEntry(key, value);
+		tempLeafNode->dataEntries.push_back(newDataEntry);
+		sort(tempLeafNode->dataEntries.begin(), tempLeafNode->dataEntries.end(), compareLeaf);
 
+		if (isLeafSplit(tempLeafNode)) { // 1개 추가한 후 blockSize를 넘는 경우, 정렬된 상태
+			IndexEntry* newIndexEntry = leafSplit(insertLocation, tempLeafNode); // 리턴 : key랑 splitblockID
+			// leaf Split = split용 블럭 생성, nextBlockBID 연결, 데이터 반씩 나누기, 파일에 기존노드, split노드 write, indexEntry 반환
+			while(1){ // nonLeaf도 진행
+				// case 1. 상위 nonLeafNode 없는 경우 생성해주고 삽입 후 종료
+				if (trackID.empty()) { 
+					int nonLeafAfterSplit = this->blockCount + 1; // 새 상위 노드용 blockID
+					makeNewBlock(nonLeafAfterSplit); // 상위 노드 생성
+					setHeader(nonLeafAfterSplit, getHeader(Header::depth) + 1); // 새로 생겼으므로 count로 rootBID, depth도 update
+					NonLeafNode* nonLeafNode = new NonLeafNode();
+					nonLeafNode->BIDpointer = insertLocation;
+					nonLeafNode->indexEntries.push_back(newIndexEntry);
+					updateNonLeafData(nonLeafAfterSplit, nonLeafNode); // BIDpointer랑 indexEntry 한꺼번에 write
+					break; // 종료
+				} // 여기까지 상위레벨 없는 경우 노드 생성, root,depth, BIDpointer와 새로운 indexEntry write까지
+				//case 2. 상위 nonLeafNode 있는 경우  삽입 후 split 여부 판단
+				else { // stack에 내 상위 레벨 노드 BID가 있는 경우 거기에 삽입을 진행한다. 계속 타고 올라가면서 split 발생 시 계속 반복
+					int tempUpBlockID = trackID.top();
+					trackID.pop();
+									//int tempUpUpBlockID = 0;
+									//if (!trackID.empty()) { // 두 레벨 위 노드의 존재 여부 확인용(있으면 split시 그곳에 insert, 없으면 new block)
+									//	tempUpUpBlockID = trackID.top();
+									//}
+					NonLeafNode* tempNonLeafNode = getNonLeafNode(tempUpBlockID); // 상위노드 가져오기
+					//IndexEntry* newIndexEntry = new IndexEntry(); // 새로 생긴 block의 첫 key 값과 blockID;
+					// 일단 위에서 만든거 사용
+					tempNonLeafNode->indexEntries.push_back(newIndexEntry); // 상위 노드에 index 추가
+					sort(tempNonLeafNode->indexEntries.begin(), tempNonLeafNode->indexEntries.end(), compareNonLeaf);
+					// split 진행하면서 안의 내용들만 바뀔뿐 -> 따라서 index를 key값에 맞춰서만 넣어주고 pointer는 자기가 가리키던거 계속
+					if (isNonLeafSplit(tempNonLeafNode)) { // 현재 삽입 + 정렬상태 -> 크기가 맞지 않으면 split
+						//split 발생 시 leaf처럼 똑같이 1/2 해주고 오른쪽 노드의 첫 번째 index entry를 위로 올려준다
+						//nonLeaf만의 과정 진행 + 또 위에다가 insert하되 없으면 newBlock 만들기, 
+						newIndexEntry = nonLeafSplit(tempUpBlockID, tempNonLeafNode); // 새로 올라가야하는 entry에 저장, write
+						// while문 돌면서 비어있다면 case1 이후 종료, 아니라면 case2로 와서 상위 노드에 삽입, 계속 진행
+					}
+					else {// 아니면 삽입된 상위노드 write해주기
+						updateNonLeafData(tempUpBlockID, tempNonLeafNode); // split 발생 안하면 insert한채로 파일 write
+						break; // split 발생 안하므로 insert 종료
+					}
+				}
+			}
 		}
-		else { // entry 1개 추가해도 split 발생하지 않는 경우
-			DataEntry *newDataEntry = new DataEntry(key, value);
-			tempLeafNode->dataEntries.push_back(newDataEntry);
-			sort(tempLeafNode->dataEntries.begin(), tempLeafNode->dataEntries.end(), compare);
-			updateLeafData(tempRootBID, tempLeafNode);
+		else { // entry 1개 추가해도 split 발생하지 않는 경우 write해주고 종료
+			updateLeafData(insertLocation, tempLeafNode);
+			return;
 		}
 	}
 	// search는 헤더에서 루트bid 가져와서 루트부터 내려가자
